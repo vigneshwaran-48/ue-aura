c8d47d89ee0dbb70f899632a9b3b035c48676e11
Vigneshwaran <p3487260@gmail.com>
2026-04-12 17:36:46 +0530

    Equipment system refactor

--- Source/Aura/Private/Equipment/AuraEquipmentManagerComponent.cpp
+++ Source/Aura/Private/Equipment/AuraEquipmentManagerComponent.cpp
@@ -5,10 +5,15 @@
 #include "Equipment/AuraEquipmentDefinition.h"
 #include "Equipment/AuraEquipmentInstance.h"
 
+UAuraEquipmentManagerComponent::UAuraEquipmentManagerComponent() {
+  PrimaryComponentTick.bCanEverTick = false;
+}
+
+void UAuraEquipmentManagerComponent::BeginPlay() { Super::BeginPlay(); }
+
 UAuraAbilitySystemComponent* UAuraEquipmentManagerComponent::GetASC() const {
   IAbilitySystemInterface* ASCInterface =
       Cast<IAbilitySystemInterface>(GetOwner());
-
   if (!ASCInterface) return nullptr;
 
   return Cast<UAuraAbilitySystemComponent>(
@@ -22,37 +27,92 @@ UAuraEquipmentInstance* UAuraEquipmentManagerComponent::EquipItem(
   UAuraAbilitySystemComponent* ASC = GetASC();
   if (!ASC) return nullptr;
 
-  const FGameplayTagContainer& NewSlots = EquipmentDefinition->SlotTags;
-
-  for (int32 i = 0; i < EquippedItems.Num(); i++) {
-    UAuraEquipmentInstance* Existing = EquippedItems[i];
-
-    if (Existing->GetEquipmentDefinition()->SlotTags.HasAny(NewSlots)) {
-      Existing->OnUnequipped(ASC);
-      EquippedItems.RemoveAt(i);
-    }
-  }
-
   UAuraEquipmentInstance* NewInstance = NewObject<UAuraEquipmentInstance>(
       GetOwner(), EquipmentDefinition->InstanceType);
 
   NewInstance->Initialize(
       const_cast<UAuraEquipmentDefinition*>(EquipmentDefinition), GetOwner());
 
-  NewInstance->OnEquipped(ASC);
+  switch (EquipmentDefinition->EquipmentType) {
+    case EAuraEquipmentType::Primary:
+      EquipPrimary(NewInstance, ASC);
+      break;
 
-  EquippedItems.Add(NewInstance);
+    case EAuraEquipmentType::Secondary:
+      EquipSecondary(NewInstance, ASC);
+      break;
+
+    case EAuraEquipmentType::Passive:
+      EquipPassive(EquipmentDefinition->PassiveSlotTag, NewInstance, ASC);
+      break;
+  }
 
   return NewInstance;
 }
 
-void UAuraEquipmentManagerComponent::UnequipItemBySlot(FGameplayTag SlotTag) {
-  UAuraAbilitySystemComponent* ASC = GetASC();
-
-  for (int32 i = EquippedItems.Num() - 1; i >= 0; --i) {
-    if (EquippedItems[i]->GetEquipmentDefinition()->SlotTags.HasTag(SlotTag)) {
-      EquippedItems[i]->OnUnequipped(ASC);
-      EquippedItems.RemoveAt(i);
-    }
+void UAuraEquipmentManagerComponent::EquipPrimary(
+    UAuraEquipmentInstance* NewInstance, UAuraAbilitySystemComponent* ASC) {
+  if (CurrentPrimaryEquipment) {
+    CleanupInstance(CurrentPrimaryEquipment, ASC);
+    CurrentPrimaryEquipment = nullptr;
   }
+
+  CurrentPrimaryEquipment = NewInstance;
+  NewInstance->OnEquipped(ASC);
+}
+
+void UAuraEquipmentManagerComponent::EquipSecondary(
+    UAuraEquipmentInstance* NewInstance, UAuraAbilitySystemComponent* ASC) {
+  if (CurrentSecondaryEquipment) {
+    CleanupInstance(CurrentSecondaryEquipment, ASC);
+    CurrentSecondaryEquipment = nullptr;
+  }
+
+  CurrentSecondaryEquipment = NewInstance;
+  NewInstance->OnEquipped(ASC);
+}
+
+void UAuraEquipmentManagerComponent::EquipPassive(
+    FGameplayTag PassiveTag, UAuraEquipmentInstance* NewInstance,
+    UAuraAbilitySystemComponent* ASC) {
+  if (PassiveEquipments.Contains(PassiveTag)) {
+    CleanupInstance(PassiveEquipments[PassiveTag], ASC);
+    PassiveEquipments.Remove(PassiveTag);
+  }
+
+  PassiveEquipments.Add(PassiveTag, NewInstance);
+  NewInstance->OnEquipped(ASC);
+}
+
+void UAuraEquipmentManagerComponent::UnequipPrimary() {
+  UAuraAbilitySystemComponent* ASC = GetASC();
+  if (!ASC || !CurrentPrimaryEquipment) return;
+
+  CleanupInstance(CurrentPrimaryEquipment, ASC);
+  CurrentPrimaryEquipment = nullptr;
+}
+
+void UAuraEquipmentManagerComponent::UnequipSecondary() {
+  UAuraAbilitySystemComponent* ASC = GetASC();
+  if (!ASC || !CurrentSecondaryEquipment) return;
+
+  CleanupInstance(CurrentSecondaryEquipment, ASC);
+  CurrentSecondaryEquipment = nullptr;
+}
+
+void UAuraEquipmentManagerComponent::UnequipPassive(FGameplayTag PassiveTag) {
+  UAuraAbilitySystemComponent* ASC = GetASC();
+  if (!ASC) return;
+
+  if (UAuraEquipmentInstance** Found = PassiveEquipments.Find(PassiveTag)) {
+    CleanupInstance(*Found, ASC);
+    PassiveEquipments.Remove(PassiveTag);
+  }
+}
+
+void UAuraEquipmentManagerComponent::CleanupInstance(
+    UAuraEquipmentInstance* Instance, UAuraAbilitySystemComponent* ASC) {
+  if (!Instance) return;
+
+  Instance->OnUnequipped(ASC);
 }
--- Source/Aura/Public/Equipment/AuraEquipmentManagerComponent.h
+++ Source/Aura/Public/Equipment/AuraEquipmentManagerComponent.h
@@ -9,9 +9,28 @@ class UAuraEquipmentDefinition;
 class UAuraEquipmentInstance;
 class UAuraAbilitySystemComponent;
 
-UCLASS(ClassGroup = (Aura), meta = (BlueprintSpawnableComponent))
+UENUM(BlueprintType)
+enum class EAuraEquipmentType : uint8 { Primary, Secondary, Passive };
+
+UCLASS(ClassGroup = (Custom), meta = (BlueprintSpawnableComponent))
 class AURA_API UAuraEquipmentManagerComponent : public UActorComponent {
   GENERATED_BODY()
+
+ public:
+  UAuraEquipmentManagerComponent();
+
+ protected:
+  virtual void BeginPlay() override;
+
+ private:
+  UPROPERTY()
+  UAuraEquipmentInstance* CurrentPrimaryEquipment = nullptr;
+
+  UPROPERTY()
+  UAuraEquipmentInstance* CurrentSecondaryEquipment = nullptr;
+
+  UPROPERTY()
+  TMap<FGameplayTag, UAuraEquipmentInstance*> PassiveEquipments;
 
  public:
   UFUNCTION(BlueprintCallable, Category = "Equipment")
@@ -19,11 +38,25 @@ class AURA_API UAuraEquipmentManagerComponent : public UActorComponent {
       const UAuraEquipmentDefinition* EquipmentDefinition);
 
   UFUNCTION(BlueprintCallable, Category = "Equipment")
-  void UnequipItemBySlot(FGameplayTag Slot);
+  void UnequipPrimary();
 
- protected:
-  UPROPERTY()
-  TArray<TObjectPtr<UAuraEquipmentInstance>> EquippedItems;
+  UFUNCTION(BlueprintCallable, Category = "Equipment")
+  void UnequipSecondary();
 
+  UFUNCTION(BlueprintCallable, Category = "Equipment")
+  void UnequipPassive(FGameplayTag PassiveTag);
+
+ private:
   UAuraAbilitySystemComponent* GetASC() const;
+
+  void EquipPrimary(UAuraEquipmentInstance* NewInstance,
+                    UAuraAbilitySystemComponent* ASC);
+  void EquipSecondary(UAuraEquipmentInstance* NewInstance,
+                      UAuraAbilitySystemComponent* ASC);
+  void EquipPassive(FGameplayTag PassiveTag,
+                    UAuraEquipmentInstance* NewInstance,
+                    UAuraAbilitySystemComponent* ASC);
+
+  void CleanupInstance(UAuraEquipmentInstance* Instance,
+                       UAuraAbilitySystemComponent* ASC);
 };
